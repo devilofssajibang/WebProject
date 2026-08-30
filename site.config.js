b0c5f49a6f46bcff61e323b09a2ea6176d23f279
devilofssajibang <lrhkuunp@naver.com>
2026-08-30 21:16:53 +0900

    Add files via upload

---
+++ site.config.js
@@ -0,0 +1,9 @@
+// 사이트 이름과 이니셜만 바꾸면 로고와 제목이 전부 자동으로 바뀝니다.
+module.exports = {
+  siteName: "기록",
+  initials: "K.R",
+  description: "일과 기록을 모아두는 개인 아카이브",
+  footerNote: "All Rights Reserved © 2026",
+  email: "hello@example.com",
+  instagram: "https://instagram.com/yourhandle"
+};
