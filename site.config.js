ead07b12362835b9931f76bbff57fb2dc9d09098
devilofssajibang <lrhkuunp@naver.com>
2026-08-31 21:48:20 +0900

    Update site initials in site.config.js

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 // 사이트 이름과 이니셜만 바꾸면 로고와 제목이 전부 자동으로 바뀝니다.
 module.exports = {
   siteName: "INAO ARCHIVE",
-  initials: "!snotanoption",
+  initials: "Juyeol Khang",
   description: "For archive my puppy",
   footerNote: "All Rights Reserved Isnotanoption © 2026",
   email: "lrhkuunp@yonsei.ac.kr",
