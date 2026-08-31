40d8ddea554df545216843039f44bcb3b7fd26bd
devilofssajibang <lrhkuunp@naver.com>
2026-08-31 21:52:22 +0900

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -10,7 +10,7 @@ export default function Sidebar({ posts }) {
       <div className="brand">
         <Link href="/" className="name">
           {config.initials}
-          <sup>®</sup> {config.siteName}
+        
         </Link>
         <div className="tagline">{config.description}</div>
       </div>
