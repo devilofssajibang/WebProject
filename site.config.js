620504774bb206793fa01665684292cf572572d9
devilofssajibang <lrhkuunp@naver.com>
2026-08-31 21:45:43 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,9 +1,9 @@
 // 사이트 이름과 이니셜만 바꾸면 로고와 제목이 전부 자동으로 바뀝니다.
 module.exports = {
-  siteName: "기록",
-  initials: "K.R",
-  description: "일과 기록을 모아두는 개인 아카이브",
-  footerNote: "All Rights Reserved © 2026",
-  email: "hello@example.com",
-  instagram: "https://instagram.com/yourhandle"
+  siteName: "INAO ARCHIVE",
+  initials: "!snotanoption",
+  description: "For archive my puppy",
+  footerNote: "All Rights Reserved Isnotanoption © 2026",
+  email: "lrhkuunp@yonsei.ac.kr",
+  instagram: "https://instagram.com/sochongsonyeon"
 };
